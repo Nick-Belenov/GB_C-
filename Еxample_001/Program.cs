ba0384d9ad2ac890f594bd6206f4fbd54760d9e9
«WorkBelenov» <belenov.n.a@gmail.com>
2022-12-03 14:59:56 +0300

    Project C# Console command + gitignore

--- Еxample_001/Program.cs
+++ Еxample_001/Program.cs
@@ -1,2 +1,7 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+//Console.WriteLine("Hello, World!");
+
+Console.Write("Enter your name: ");
+string username = Console.ReadLine();
+Console.Write("Hello, ");
+Console.Write(username);
