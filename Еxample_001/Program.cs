bf3a96e006a997c18f4c36f7a812b7f8fae9aebf
BelenovDell <belenov.n.a@gmail.com>
2022-12-04 15:03:32 +0300

    add while

--- Еxample_001/Program.cs
+++ Еxample_001/Program.cs
@@ -102,7 +102,7 @@ else
     Console.Write("Привет, ");
     Console.WriteLine(username);
 }
-*/
+
 
 int a = 8;
 int b = 7;
@@ -119,3 +119,49 @@ if(e > max) max = e;
 
 Console.Write("max = ");
 Console.WriteLine(max);
+
+
+Console.Clear();
+//Console.SetCursorPosition(10, 4);  //Комманда назначения позиции курсора с отступами
+//Console.WriteLine("*");
+
+int xa = 20,ya = 1,
+    xb = 1,yb = 15,
+    xc = 40,yc = 15;
+
+Console.SetCursorPosition(xa,ya);
+Console.WriteLine("*");
+
+Console.SetCursorPosition(xb,yb);
+Console.WriteLine("*");
+
+Console.SetCursorPosition(xc,yc);
+Console.WriteLine("*");
+
+int x = xa, y = xb;
+int count = 0;
+
+while(count < 10000)
+{
+    int what = new Random().Next(0, 3);  // 0 1 2
+    if(what == 0)
+    {
+        x = (x + xa) / 2;
+        y = (y + ya) / 2;
+    }
+        if(what == 1)
+    {
+        x = (x + xb) / 2;
+        y = (y + yb) / 2;
+    }
+        if(what == 2)
+    {
+        x = (x + xc) / 2;
+        y = (y + yc) / 2;
+    }
+
+Console.SetCursorPosition(x, y);
+Console.WriteLine("*");
+count = count + 1;   // Если всегда увиличивать на 1, то будет достаточно "count++"
+}
+*/
