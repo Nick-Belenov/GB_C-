4f41863505952b52d6d1495ba1646557746a262c
BelenovDell <belenov.n.a@gmail.com>
2022-12-04 14:21:06 +0300

    add new project

--- Еxample_001/Program.cs
+++ Еxample_001/Program.cs
@@ -104,4 +104,18 @@ else
 }
 */
 
+int a = 8;
+int b = 7;
+int c = 1;
+int d = 4;
+int e = 5;
 
+int max = a;
+if(a > max) max = a;
+if(b > max) max = b;
+if(c > max) max = c;
+if(d > max) max = d;
+if(e > max) max = e;
+
+Console.Write("max = ");
+Console.WriteLine(max);
